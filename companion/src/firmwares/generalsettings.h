98b34fdf2c9a5b1d5a1989c8c0a883a9d2d2a270
Paul Tapper <paul@tapper.com>
2023-04-02 14:28:35 +0100

    Companion support for power off after inactivity

--- companion/src/firmwares/generalsettings.h
+++ companion/src/firmwares/generalsettings.h
@@ -302,6 +302,7 @@ class GeneralSettings {
     bool modelSFDisabled;
     bool modelCustomScriptsDisabled;
     bool modelTelemetryDisabled;
+    unsigned int pwrOffIfInactive;
 
     bool switchPositionAllowedTaranis(int index) const;
     bool switchSourceAllowedTaranis(int index) const;
