7d101c3ef332c4e7e4bad690ccde5c0c93e954c6
Paul Tapper <paul@tapper.com>
2023-07-14 15:12:18 +0000

    Yaml parsers updated for radio firmware

--- radio/src/storage/yaml/yaml_datastructs_x12s.cpp
+++ radio/src/storage/yaml/yaml_datastructs_x12s.cpp
@@ -399,6 +399,7 @@ static const struct YamlNode struct_RadioData[] = {
   YAML_UNSIGNED( "modelSFDisabled", 1 ),
   YAML_UNSIGNED( "modelCustomScriptsDisabled", 1 ),
   YAML_UNSIGNED( "modelTelemetryDisabled", 1 ),
+  YAML_UNSIGNED( "pwrOffIfInactive", 8 ),
   YAML_END
 };
 static const struct YamlNode struct_unsigned_8[] = {
